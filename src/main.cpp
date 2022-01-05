80ac99a1c7583041a196ecf1b71c5ed122616b1e
Marcel Fischer <Fischerboot-sinkt@web.de>
2022-01-05 21:22:42 +0100

    Build ok, Running on Target, OTA working, logger working

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@
 
 #include <Arduino.h>
 #include <EspMultiLogger.h>
+#include <ArduinoOTA.h>
 #include "WlanConfig.h"
 /*
 Configuration
 */
-const char* versionStr = "20220105v0.3";
+const char* versionStr = "20220105v0.4";
 
 #define LED 2
 
@@ -21,22 +22,83 @@ const char* ssid = STASSID;
 const char* password = STAPSK;
 
 EspMultiLogger* Logger;
-
+bool on = true;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.print("\n Starting Version:");
   Serial.println(versionStr);
   pinMode(LED, OUTPUT);
+  // W-Lan Activating
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.print("local ip: ");
+  Serial.println(WiFi.localIP());
 
   Logger = new EspMultiLogger(Debug);
   Logger->setLogLevel(Debug);
   EspMultiLogger::initLogger();
+
+  // OTA Begin
+    // OTA (only after connection is established)
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("fischerboot_Generic");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  //OTA End
 }
 
 void loop() {
-  bool on = true;
+  // Begin Generic loop
+  ArduinoOTA.handle();
   EspMultiLogger::loopLogger();
+  // End Generic loop
   // put your main code here, to run repeatedly:
   if(millis()%1000==0){
     if(on == true){
